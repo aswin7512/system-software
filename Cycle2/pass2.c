ffe79e10a73fec3956f7886275c331b46665d150
Aswin <aswinnrd09@gmail.com>
2025-08-17 01:23:35 +0530

    pass2 in the middle

--- Cycle2/pass2.c
+++ Cycle2/pass2.c
@@ -3,27 +3,61 @@
 #include<string.h>
 
 #define BUF 20
-#define MAX 50
+#define MAX 100
 
-typedef struct rec {
-	char tag;
-	int len;
-	char rec[60];
-} record;
-
-typedef struct tab{
+typedef struct tab {
     char symbol[BUF];
     int address;
 } tab;
 
-int otsize = 0, stsize = 0;
+typedef struct inter {
+	int addr;
+	char label[BUF];
+	char opcode[BUF];
+	char opnd[BUF];
+} inter;
+
+int otsize = 0, stsize = 0, lcnt = 0;;
 tab optab[BUF];
 tab symtab[BUF];
+inter itr[MAX];
 
 
-
-void pass2(int addr, char label[], char ocode[], char opnd[]) {
-	if (strcmp(ocode, "START"));
+void pass2(FILE* out, FILE* list) {
+	fprintf(out, "H %6s %6X %6X\n", itr[0].label, itr[0].addr, itr[lcnt].addr - itr[0].addr);
+	fprintf(list, "%X\t%s\t%s\t%s\t**\n", itr[0].addr, itr[0].label, itr[0].opcode, itr[0].opnd);
+	int trec = 0;
+	for (int i = 1; i < lcnt; i++) {
+		int found = -1;
+		for (int j = 0; j < otsize; j++) {
+			if (!strcmp(optab[j].symbol, itr[i].opcode)) {
+				found = j;
+				break;
+			}
+		}
+		if (found != -1) {
+			char obj[6], tmp[4];
+			sprintf(obj, "%X", optab[found].address);
+			if (strcmp(itr[i].opnd, "**")) {
+				int stf = -1;
+				for (int j = 0; j < stsize; j++) {
+					if (strcmp(symtab[j].symbol, itr[i].opnd)) {
+						stf = j;
+						break;
+					}
+				}
+				if (stf != -1)
+					sprintf(tmp, "%X", symtab[stf].address);
+				else {
+					printf("Undefined Symbol!!!\n");
+					exit(1);
+				}
+			} else {
+				sprintf(tmp, "%X", 0000);
+			}
+			strcat(obj, tmp);		//done till object code formation for opcode operand pair and need to form the text record....
+		}
+	}
 }
 
 
@@ -31,30 +65,16 @@ int main() {
     FILE* inter = fopen("intermediate.sic", "r");
     FILE* otab = fopen("optab", "r");
     FILE* stab = fopen("symtab", "r");
-	FILE* rec = fopen("record", "w");
-    if (!inter || !otab || !stab || !rec) {
+	FILE* out = fopen("output", "w");
+	FILE* list = fopen("listfile.sic", "2");
+    if (!inter || !otab || !stab || !out || !list) {
         printf("Couldnot Open Files...\n");
         return 1;
     }
-    char opcode[BUF], op, symbol[BUF];
-	int opint, sint;
-    while (fscanf(otab, "%s\t%d", opcode, &opint) > 0) {
-		strcpy(optab[otsize].symbol, opcode);
-		optab[otsize++].address = opint;
-	}
-	while (fscanf(stab, "%s\t%d", symbol, &sint) > 0) {
-		strcpy(symtab[stsize].symbol, symbol);
-		symtab[stsize++].address = sint;
-	}
-	char label[BUF], ocode[BUF], opnd[BUF];
-	int addr;
 
+    while (fscanf(otab, "%s\t%d", optab[otsize].symbol, &optab[otsize++].address) > 0);			//read optab to memory...
+	while (fscanf(stab, "%s\t%d", symtab[stsize].symbol, &symtab[stsize++].address) > 0);		//read symtab to memory...
+	while (fscanf(inter, "%X\t%s\t%s\t%s\t", &itr[lcnt].addr, itr[lcnt].label, itr[lcnt].opcode, itr[lcnt].opnd) > 0);		//read intermediate file to memory...
 
-	do {
-		fscanf(inter, "%X\t%s\t%s\t%s\n", &addr, label, ocode, opnd);
-		pass2(addr, label, ocode, opnd);
-	} while (strcmp(ocode, "END"));
-	for (int i = 0; i < otsize; i++)
-		printf("%s: %X\n", optab[i].symbol, optab[i].address);
-    return 0;
+	pass2(out, list);
 }
